3de476e958b765699eb2f98ca42336acfc262f49
Anton Heestand <anton.heestand@gmail.com>
2023-10-01 18:29:38 +0900

    Frame Generator Cleanup

--- Sources/VideoFrames/ToFrames.swift
+++ Sources/VideoFrames/ToFrames.swift
@@ -129,17 +129,16 @@ public func videoFrame(at frameIndex: Int, from url: URL, info: VideoInfo? = nil
 func getFrame(at frameIndex: Int, info: VideoInfo, with generator: AVAssetImageGenerator) async throws -> _Image {
     let time: CMTime = CMTime(value: CMTimeValue(frameIndex * 1_000_000),
                               timescale: CMTimeScale(info.fps * 1_000_000))
+#if os(visionOS)
+    let (cgImage, _): (CGImage, CMTime) = try await generator.image(at: time)
+#else
     let cgImage: CGImage
     if #available(iOS 16, macOS 13, visionOS 1.0, *) {
-        let (image, _): (CGImage, CMTime) = try await generator.image(at: time)
-        cgImage = image
+        (cgImage, _) = try await generator.image(at: time)
     } else {
-        #if os(visionOS)
-        fatalError()
-        #else
         cgImage = try generator.copyCGImage(at: time, actualTime: nil)
-        #endif
     }
+#endif
     #if os(macOS)
     let image: NSImage = NSImage(cgImage: cgImage, size: info.size)
     #else
